d109ab79ac81496a23a6c26344b403e5f750dd74
Amir Kasumovic <amir.kasumovic@live.com>
2021-11-15 13:19:26 +0100

    responsiveness

--- src/App.style.ts
+++ src/App.style.ts
@@ -1,5 +1,5 @@
-import styled, {createGlobalStyle} from 'styled-components';
-import BGImage from './img/background.jpg';
+import styled, { createGlobalStyle } from "styled-components";
+import BGImage from "./img/background.jpg";
 
 export const GlobalStyle = createGlobalStyle`
     html {
@@ -12,6 +12,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0 20px;
         display: flex;
         justify-content: center;
+        width: 100%;
     }
 
     * {
@@ -20,56 +21,65 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Encode Sans SC', sans-serif;
     }
 
+    .App {
+        width: 100%;
+    }
 `;
 
 export const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
 
-    > p {
-        color: #fff;
-    }
-    
-    .score {
-        color: #fff;
-        font-size: 2rem;
-        margin: 0 auto;
-    }
+  > p {
+    color: #fff;
+  }
 
-    h1 {
-        font-family: 'Tourney', cursive;
-        background-image: linear-gradient(180deg, #fff, #87f1ff);
-        background-size: 100%;
-        background-clip: text;
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-        -moz-background-clip: text;
-        -moz-text-fill-color: transparent;
-        filter: drop-shadow(2px 2px #0085a3);
-        font-size: 70px;
-        text-align: center;
-        margin: 20px;
-    }
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0 auto;
+  }
 
-    .start, .next {
-        cursor: pointer;
-        background: linear-gradient(180deg, rgba(0, 0, 0, 0.3), rgba(255, 255, 255, 0.2));
-        border: 1px solid #0085a3;
-        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
-        border-radius: 10px;
-        font-size: 1rem;
-        height: 40px;
-        margin: 20px 0;
-        padding: 0 40px;
-    }
+  h1 {
+    font-family: "Tourney", cursive;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+  }
 
-    .start {
-        min-width: 200px;
-    }
+  .start,
+  .next {
+    cursor: pointer;
+    background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.3),
+      rgba(255, 255, 255, 0.2)
+    );
+    border: 1px solid #0085a3;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    font-size: 1rem;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
 
-    .loading{
-        margin-top: 70px;
-        font-size: 2rem;
-    }
-`
+  .start {
+    min-width: 200px;
+  }
+
+  .loading {
+    margin-top: 70px;
+    font-size: 2rem;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import { useState } from 'react';
+import { useState } from "react";
 //components
-import QuestionCard from './components/question/questionCard';
+import QuestionCard from "./components/question/questionCard";
 //api
-import { fetchQuizQuestions } from './api';
+import { fetchQuizQuestions } from "./api";
 //types
-import { Difficulty, QuestionState } from './api';
+import { Difficulty, QuestionState } from "./api";
 //styles
-import {GlobalStyle, Wrapper} from './App.style'
+import { GlobalStyle, Wrapper } from "./App.style";
 
 const TOTAL_QUESTIONS = 10;
 
@@ -17,7 +17,7 @@ export type AnswerObject = {
   answer: string;
   correct: boolean;
   correctAnswer: string;
-}
+};
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
@@ -25,82 +25,197 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+  const TOTAL_QUESTIONS = 11;
 
   console.log(questions);
+
+  const newQuestions = [
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+    {
+      answers: ["Blood", "Suggar", "Sex", "Magic"],
+      category: "Crypto",
+      correct_answer: "Blood",
+      difficulty: "easy",
+      incorrect_answers: ["Suggar", "Sex", "Magic"],
+      question: "Which one is correct?",
+      type: "multiple",
+    },
+  ];
 
   const startTrivia = async () => {
     setLoading(true);
     setGameOver(false);
 
-    const newQuestions = await fetchQuizQuestions(
-      TOTAL_QUESTIONS,
-      Difficulty.EASY
-    );
+    // const newQuestions = await fetchQuizQuestions(
+    //   TOTAL_QUESTIONS,
+    //   Difficulty.EASY
+    // );
 
     setQuestions(newQuestions);
     setScore(0);
     setUserAnswers([]);
     setNumber(0);
     setLoading(false);
-  }
-  
+    console.log(newQuestions);
+  };
+
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if(!gameOver){
+    if (!gameOver) {
       //User answer
-      const answer = e.currentTarget.value
+      const answer = e.currentTarget.value;
       //check answer against correct answer
       const correct = questions[number].correct_answer === answer;
-      if(correct) setScore((prev)=> prev +1);
+      if (correct) setScore((prev) => prev + 1);
       //save answer in the array for user answer
       const answerObject = {
-        question : questions[number].question,
+        question: questions[number].question,
         answer,
-        correct, 
-        correctAnswer: questions[number].correct_answer
+        correct,
+        correctAnswer: questions[number].correct_answer,
       };
-      setUserAnswers((prev)=> [...prev, answerObject]);
+      setUserAnswers((prev) => [...prev, answerObject]);
     }
-  }
+  };
 
   const nextQuestion = () => {
     //move on to the next question if not the last one
-    const nextQuestion = number +1;
+    const nextQuestion = number + 1;
 
-    if(nextQuestion === TOTAL_QUESTIONS){
+    if (nextQuestion === TOTAL_QUESTIONS) {
       setGameOver(true);
-    }else {
+    } else {
       setNumber(nextQuestion);
     }
-  }
+  };
 
   return (
     <>
-    <GlobalStyle />
-    <div className="App">
-    <Wrapper>
-        <h1>QUIZ</h1>
-        {gameOver || userAnswers.length === TOTAL_QUESTIONS ?(
-          <button className="start" onClick={startTrivia}>START</button>
-        ) : null}
-        {!gameOver ? <p className="score"><strong>SCORE: {score}</strong>  </p> : null}
-        {loading && <p className="loading">Loading Questions...</p>}
-        {!loading && !gameOver && (
-          <QuestionCard 
-          questionNumber={number +1} 
-          totalQuestions={TOTAL_QUESTIONS} 
-          question={questions[number].question} 
-          answers={questions[number].answers} 
-          userAnswer={userAnswers ? userAnswers[number]: undefined}
-          callback={checkAnswer}
-          />
-        )}
-        {!loading && !gameOver && userAnswers.length === number +1 && number !== TOTAL_QUESTIONS -1 ?(
-          <button className="next" onClick={nextQuestion}>Next</button>
-        ) : null}
+      <GlobalStyle />
+      <div className='App'>
+        <Wrapper>
+          <h1 className='quizTitle'>CRYPTO QUIZ</h1>
+          {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+            <button className='start' onClick={startTrivia}>
+              START
+            </button>
+          ) : null}
+          {!gameOver ? (
+            <p className='score'>
+              <strong>SCORE: {score}</strong>{" "}
+            </p>
+          ) : null}
+          {loading && <p className='loading'>Loading Questions...</p>}
+          {!loading && !gameOver && (
+            <QuestionCard
+              questionNumber={number + 1}
+              totalQuestions={TOTAL_QUESTIONS}
+              question={questions[number].question}
+              answers={questions[number].answers}
+              userAnswer={userAnswers ? userAnswers[number] : undefined}
+              callback={checkAnswer}
+            />
+          )}
+          {!loading &&
+          !gameOver &&
+          userAnswers.length === number + 1 &&
+          number !== TOTAL_QUESTIONS - 1 ? (
+            <button className='next' onClick={nextQuestion}>
+              Next
+            </button>
+          ) : null}
         </Wrapper>
       </div>
     </>
   );
-}
+};
 
 export default App;
